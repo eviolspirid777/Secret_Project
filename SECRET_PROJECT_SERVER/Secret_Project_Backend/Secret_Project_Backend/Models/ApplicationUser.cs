152d5be1bf27a3f5b95ad218cb6ca1a148ac7c99
eviolspirid <eviolspirid@gmail.com>
2025-05-28 00:05:38 +0300

    Исправлены warnings

--- SECRET_PROJECT_SERVER/Secret_Project_Backend/Secret_Project_Backend/Models/ApplicationUser.cs
+++ SECRET_PROJECT_SERVER/Secret_Project_Backend/Secret_Project_Backend/Models/ApplicationUser.cs
@@ -4,10 +4,10 @@ namespace Secret_Project_Backend.Models
 {
     public class ApplicationUser: IdentityUser
     {
-        public string DisplayName { get; set; }
+        public string DisplayName { get; set; } = string.Empty;
         public string? AvatarUrl { get; set; } = null;
 
-        public virtual ICollection<Message> Messages { get; set; }
-        public virtual ICollection<ChannelUser> ChannelUsers { get; set; }
+        public virtual ICollection<Message> Messages { get; set; } = new List<Message>();
+        public virtual ICollection<ChannelUser> ChannelUsers { get; set; } = new List<ChannelUser>();
     }
 }
