2e29921b714d77ebc2825a805c88580669d0c885
Veh99 <sasmemologov@mail.ru>
2026-03-03 18:44:10 +0300

    Новые изменения при переходе на ветку

--- SECRET_PROJECT_SERVER/Secret_Project_Backend/SecretProject.Service.Email/obj/Debug/net10.0/SecretProject.Service.Email.AssemblyInfo.cs
+++ SECRET_PROJECT_SERVER/Secret_Project_Backend/SecretProject.Service.Email/obj/Debug/net10.0/SecretProject.Service.Email.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecretProject.Service.Email")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43fcd6312d7841ae259b7735cbb66030b56dc1d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cb1aa6122922d3f0b89bea55d0406e745ac251")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecretProject.Service.Email")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecretProject.Service.Email")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SECRET_PROJECT_SERVER/Secret_Project_Backend/SecretProject.Service.HttpGateway.Web/obj/Debug/net10.0/SecretProject.Service.HttpGateway.Web.AssemblyInfo.cs
+++ SECRET_PROJECT_SERVER/Secret_Project_Backend/SecretProject.Service.HttpGateway.Web/obj/Debug/net10.0/SecretProject.Service.HttpGateway.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecretProject.Service.HttpGateway.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b0850dd915b587a8d72b73adbcb15d65c4130df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cb1aa6122922d3f0b89bea55d0406e745ac251")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecretProject.Service.HttpGateway.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecretProject.Service.HttpGateway.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
