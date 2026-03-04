412ccf4186154c1c43e92cf3092faf3d88413c03
Veh99 <sasmemologov@mail.ru>
2026-03-04 11:38:51 +0300

    cache

--- SECRET_PROJECT_SERVER/Secret_Project_Backend/SecretProject.Service.HttpGateway.Web/obj/Debug/net10.0/SecretProject.Service.HttpGateway.Web.AssemblyInfo.cs
+++ SECRET_PROJECT_SERVER/Secret_Project_Backend/SecretProject.Service.HttpGateway.Web/obj/Debug/net10.0/SecretProject.Service.HttpGateway.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecretProject.Service.HttpGateway.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f3bddffd39b2f5e6bb5dbcca2e9c1402b085563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a06671adc24aaee44a6719ff8b87998a2bd1f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecretProject.Service.HttpGateway.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecretProject.Service.HttpGateway.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
